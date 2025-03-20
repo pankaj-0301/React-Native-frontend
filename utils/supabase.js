24aa1e4e062cea517100d918af304d75a92be53f
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2025-03-21 00:07:16 +0530

    Update supabase.js

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your environment variables
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://xhkzhhbjqvevngswirur.supabase.co';
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoa3poaGJqcXZldm5nc3dpcnVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxMDEzNjgsImV4cCI6MjA1NzY3NzM2OH0.lztvDv8VUHotHTayQJhUr2VVOOKuTqMZ9kWemeDz4PQ';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
